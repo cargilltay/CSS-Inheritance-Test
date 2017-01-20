76050137333991c0569d9dcc7f80e55ddad60da8
Taylor Cargill <tcargill@bizstream.com>
2017-01-19 22:06:52 -0500

    setup test dom via js

--- js-flow.js
+++ js-flow.js
@@ -1,7 +1,18 @@
 jQuery(function($) {
 
-	var init = function() {
+	var MAX_ELEMENTS = 25;
 
+	var init = function() {
+			setupTestDom();
+	}
+
+	var setupTestDom = function() {
+			for (var i = 0; i < MAX_ELEMENTS - 1; i++){
+				$('<div class="row">' +
+
+
+					'</div>').appendTo($('#container-main'));
+			}
 	}
 
 	init();
